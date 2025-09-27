ab675b291da4ace8e3cde533191be2659b66ff72
willy-murimi <149483454+willy-murimi@users.noreply.github.com>
2025-09-27 04:09:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
 #!bin/python3
 import requests
 import BeautifulSoup as bs4
+
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+url = input('Enter the url you wish to use')
+requests = request.get(url)
+response = requests.response
+port = input('Enter the port you wish to use for connection')
+socket.connect(url, port)
+
+if response.status_code == 200:
+  while True:
+    data = sock.recv(4096)
+    print(data)
+  if not data:
+    break
+  print(data.decode('utf8' errors='ignore')
+  sock.close()
+
